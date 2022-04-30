739fd4d9c208b92f765d036d6e1c6d86f13d9259
Ben Sommer <32485726+ben-sommer@users.noreply.github.com>
2022-04-30 01:11:42 +0100

    feat: add moving, capturing

--- game.js
+++ game.js
@@ -28,14 +28,14 @@ exports.DraughtsGame = class DraughtsGame {
     const empty = Array(8).fill([0, false]);
 
     this.board = [
-      alternatingOne,
-      alternatingOne.slice().reverse(),
-      alternatingOne,
-      empty,
-      empty,
-      alternatingTwo.slice().reverse(),
-      alternatingTwo,
-      alternatingTwo.slice().reverse(),
+      [...alternatingOne],
+      [...alternatingOne.slice().reverse()],
+      [...alternatingOne],
+      [...empty],
+      [...empty],
+      [...alternatingTwo.slice().reverse()],
+      [...alternatingTwo],
+      [...alternatingTwo.slice().reverse()],
     ];
   }
 
@@ -63,4 +63,186 @@ exports.DraughtsGame = class DraughtsGame {
     console.log(output);
   }
 
+  getPossibleMoves(player) {
+    let possibleMoves = [];
+
+    this.board.forEach((row, rowIndex) => {
+      row.forEach((location, locationIndex) => {
+        if (location[0] === player) {
+          const possibleMovesForLocation = this.getPossibleMovesForLocation(
+            rowIndex,
+            locationIndex
+          );
+          possibleMoves.push(...possibleMovesForLocation);
+        }
+      });
+    });
+
+    return possibleMoves;
+  }
+
+  getPossibleMovesForLocation(rowIndex, locationIndex) {
+    let possibleMoves = [];
+
+    const location = this.board[rowIndex][locationIndex];
+
+    this.getPossibleMovesForRegular(rowIndex, locationIndex, possibleMoves);
+
+    const possibleMovesForLocation = [
+      ...(location[1] || player === 2
+        ? [
+            [rowIndex - 1, locationIndex - 1],
+            [rowIndex - 1, locationIndex + 1],
+          ]
+        : []),
+      ...(location[1] || player === 1
+        ? [
+            [rowIndex + 1, locationIndex - 1],
+            [rowIndex + 1, locationIndex + 1],
+          ]
+        : []),
+    ];
+
+    possibleMovesForLocation.forEach(([newRowIndex, newLocationIndex]) => {
+      if (
+        this.isValidMove(rowIndex, locationIndex, newRowIndex, newLocationIndex)
+      ) {
+        possibleMoves.push([
+          [rowIndex, locationIndex],
+          [newRowIndex, newLocationIndex],
+        ]);
+      }
+    });
+
+    return possibleMoves;
+  }
+
+  isValidMove(rowIndex, locationIndex, newRowIndex, newLocationIndex, player) {
+    if (
+      newRowIndex < 0 ||
+      newRowIndex > 7 ||
+      newLocationIndex < 0 ||
+      newLocationIndex > 7 ||
+      rowIndex < 0 ||
+      rowIndex > 7 ||
+      locationIndex < 0 ||
+      locationIndex > 7
+    ) {
+      return false;
+    }
+
+    const location = this.board[rowIndex][locationIndex];
+
+    if (location[0] !== player) {
+      return false;
+    }
+
+    const newLocation = this.board[newRowIndex][newLocationIndex];
+
+    if (newLocation[0] !== 0) {
+      return false;
+    }
+
+    if (
+      Math.abs(rowIndex - newRowIndex) === 2 &&
+      Math.abs(locationIndex - newLocationIndex) === 2
+    ) {
+      const possibleMovesForLocation = [
+        ...(location[1] || player === 2
+          ? [
+              [rowIndex - 2, locationIndex - 2],
+              [rowIndex - 2, locationIndex + 2],
+            ]
+          : []),
+        ...(location[1] || player === 1
+          ? [
+              [rowIndex + 2, locationIndex - 2],
+              [rowIndex + 2, locationIndex + 2],
+            ]
+          : []),
+      ];
+
+      if (
+        !possibleMovesForLocation.filter(
+          (x) => x[0] === newRowIndex && x[1] === newLocationIndex
+        ).length
+      ) {
+        return false;
+      }
+
+      const midRowIndex = (rowIndex + newRowIndex) / 2;
+      const midLocationIndex = (locationIndex + newLocationIndex) / 2;
+
+      const midLocation = this.board[midRowIndex][midLocationIndex];
+
+      if (midLocation[0] === player || midLocation[0] === 0) {
+        return false;
+      }
+
+      return true;
+    } else {
+      const possibleMovesForLocation = [
+        ...(location[1] || player === 2
+          ? [
+              [rowIndex - 1, locationIndex - 1],
+              [rowIndex - 1, locationIndex + 1],
+            ]
+          : []),
+        ...(location[1] || player === 1
+          ? [
+              [rowIndex + 1, locationIndex - 1],
+              [rowIndex + 1, locationIndex + 1],
+            ]
+          : []),
+      ];
+
+      if (
+        !possibleMovesForLocation.filter(
+          (x) => x[0] === newRowIndex && x[1] === newLocationIndex
+        ).length
+      ) {
+        return false;
+      }
+
+      if (
+        Math.abs(rowIndex - newRowIndex) !== 1 ||
+        Math.abs(locationIndex - newLocationIndex) !== 1
+      ) {
+        return false;
+      }
+
+      return true;
+    }
+  }
+
+  move(rowIndex, locationIndex, newRowIndex, newLocationIndex, player) {
+    if (
+      !this.isValidMove(
+        rowIndex,
+        locationIndex,
+        newRowIndex,
+        newLocationIndex,
+        player
+      )
+    ) {
+      return false;
+    }
+
+    if (
+      Math.abs(rowIndex - newRowIndex) === 2 &&
+      Math.abs(locationIndex - newLocationIndex) === 2
+    ) {
+      this.board[newRowIndex][newLocationIndex] =
+        this.board[rowIndex][locationIndex];
+      this.board[rowIndex][locationIndex] = [0, false];
+      this.board[(rowIndex + newRowIndex) / 2][
+        (locationIndex + newLocationIndex) / 2
+      ] = [0, false];
+    } else {
+      this.board[newRowIndex][newLocationIndex] =
+        this.board[rowIndex][locationIndex];
+      this.board[rowIndex][locationIndex] = [0, false];
+    }
+    return true;
+  }
 };
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const { DraughtsGame } = require("./game");
 const game = new DraughtsGame();
 
 game.showBoard();
+console.log(game.move(2, 3, 3, 4, 1));
+console.log(game.move(5, 2, 4, 3, 2));
+console.log(game.move(3, 4, 5, 2, 1));
+console.log(game.move(6, 1, 4, 3, 2));
+game.showBoard();
